eddb197f6dafada01dc2e59716a7625c0ae4e388
Luciane Santos <lucianecarvalho@gmail.com>
2018-09-14 13:54:17 -0300

    Adicionado Evento da Unidade

---
+++ WebMetasV2/Rumo.WebMetasV2.Domain/Events/UnidadeRegisteredEvent.cs
@@ -0,0 +1,19 @@
+﻿using Rumo.WebMetasV2.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rumo.WebMetasV2.Domain.Events
+{
+    public class UnidadeRegisteredEvent : Event
+    {
+        public UnidadeRegisteredEvent(Guid id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ WebMetasV2/Rumo.WebMetasV2.Domain/Events/UnidadeRemovedEvent.cs
@@ -0,0 +1,17 @@
+﻿using Rumo.WebMetasV2.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rumo.WebMetasV2.Domain.Events
+{
+    public class UnidadeRemovedEvent : Event
+    {
+        public UnidadeRemovedEvent(Guid id)
+        {
+            Id = id;
+            AggregateId = id;
+        }
+        public Guid Id { get; set; }
+    }
+}
