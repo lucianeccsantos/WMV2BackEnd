3ebd1f1ec7036ca5e1ce2fd00008fe22428f7aeb
Luciane Santos <lucianecarvalho@gmail.com>
2018-09-17 10:41:17 -0300

    Adicionado Controller de Unidade

---
+++ WebMetasV2/Rumo.WebMetasV2.Application/Interfaces/IUnidadeAppService.cs
@@ -0,0 +1,18 @@
+﻿using Rumo.WebMetasV2.Application.ViewModels;
+using Rumo.WebMetasV2.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rumo.WebMetasV2.Application.Interfaces
+{
+    public interface IUnidadeAppService : IDisposable
+    {
+        void Register(UnidadeViewModel unidadeViewModel);
+        IEnumerable<UnidadeViewModel> GetAll();
+        UnidadeViewModel GetById(Guid id);
+        void Update(UnidadeViewModel unidadeViewModel);
+        PagedResult<UnidadeViewModel> ListForPaging(int page, int pagesize);
+        void Remove(Guid id);
+    }
+}
---
+++ WebMetasV2/Rumo.WebMetasV2.Application/ViewModels/UnidadeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rumo.WebMetasV2.Application.ViewModels
+{
+    public class UnidadeViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
